8e0d87623afbd2ede0e1d6cf866d5ac85915f0fe
frc <fcaumartin@gmail.com>
2016-02-29 14:11:56 +0100

    crud complet

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -31,59 +31,122 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 27);
+            this.button1.Location = new System.Drawing.Point(151, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Ajouter";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(30, 89);
+            this.button2.Location = new System.Drawing.Point(151, 41);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Modifier";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(131, 27);
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(116, 199);
+            this.listBox1.Size = new System.Drawing.Size(116, 238);
             this.listBox1.TabIndex = 2;
             // 
-            // dataGridView1
+            // button3
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(266, 27);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(377, 199);
-            this.dataGridView1.TabIndex = 3;
+            this.button3.Location = new System.Drawing.Point(151, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Supprimer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(248, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 237);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(69, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(157, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "OK";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(134, 197);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Annuler";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 261);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(501, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -93,7 +156,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -14,6 +14,8 @@ namespace GUI
 {
     public partial class Form1 : Form
     {
+
+        string action = "";
         public Form1()
         {
             InitializeComponent();
@@ -21,40 +23,110 @@ namespace GUI
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            ClientDAO data = new ClientDAO();
+
+            listBox1.DisplayMember = "Nom";
+            listBox1.ValueMember = "Id";
+            listBox1.DataSource = data.List();
+            Width = 254;
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Client c = new Client();
-            c.Nom = "Toto";
-            c.Prenom = "Titi";
-            c.Ville = "Amiens";
 
-            ClientDAO database = new ClientDAO();
-
-            database.Insert(c);
+            action = "ajouter";
+            Width = 514;
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ClientDAO data = new ClientDAO();
+            if (listBox1.SelectedIndex != -1)
+            {
+                action = "modifier";
+                Width = 514;
 
-            listBox1.DisplayMember = "Nom";
-            listBox1.DataSource = data.List();
+                int id = (int)listBox1.SelectedValue;
 
-            dataGridView1.DataSource = data.List();
-            dataGridView1.Columns[0].Visible = false;
-            dataGridView1.RowHeadersVisible = false;
-            dataGridView1.ReadOnly = true;
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                ClientDAO data = new ClientDAO();
+
+                Client cli = data.Find(id);
+
+                textBox1.Text = cli.Nom;
+                textBox2.Text = cli.Prenom;
+                textBox3.Text = cli.Ville;
+
+
+            }
 
         }
-    
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (action == "ajouter")
+            {
+                Client c = new Client();
+                c.Nom = textBox1.Text;
+                c.Prenom = textBox2.Text;
+                c.Ville = textBox3.Text;
+
+                ClientDAO data = new ClientDAO();
+
+                data.Insert(c);
+
+                listBox1.DataSource = data.List();
+            }
+            if (action == "modifier")
+            {
+                Client c = new Client();
+                c.Nom = textBox1.Text;
+                c.Prenom = textBox2.Text;
+                c.Ville = textBox3.Text;
+                c.Id = (int)listBox1.SelectedValue;
+
+                ClientDAO data = new ClientDAO();
+
+                data.Update(c);
+
+                listBox1.DataSource = data.List();
+            }
+            if (action == "supprimer")
+            {
+                Client c = new Client();
+                c.Nom = textBox1.Text;
+                c.Prenom = textBox2.Text;
+                c.Ville = textBox3.Text;
+                c.Id = (int)listBox1.SelectedValue;
+
+                ClientDAO data = new ClientDAO();
+
+                data.Delete(c);
+
+                listBox1.DataSource = data.List();
+            }
+            Width = 254;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                action = "supprimer";
+                Width = 514;
+
+                int id = (int)listBox1.SelectedValue;
+
+                ClientDAO data = new ClientDAO();
+
+                Client cli = data.Find(id);
+
+                textBox1.Text = cli.Nom;
+                textBox2.Text = cli.Prenom;
+                textBox3.Text = cli.Ville;
 
 
+            }
+        }
     }
 }
